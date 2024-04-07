6edd14782390f72d7113a0bb3776a32975bd7eff
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-04-07 16:57:26 +0530

    sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
   return (
     <div>
       <p>Dashboard (Protected)</p>
-      <UserButton afterSignOutUrl="/"/> //to landing page
     </div>
   );
 }
