d54bf105fd6f139104779b034f501a8c5ab83012
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-04-07 19:00:53 +0530

    added md menu sheet

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,12 @@
 export default function DashboardPage() {
   return (
     <div>
-      <p>Dashboard (Protected)</p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of PolyGen AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">Chat and Craft with the smartest AI.</p>
+      </div>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,10 @@
-import { Menu } from "lucide-react";
-import { Button } from "./ui/button";
 import { UserButton } from "@clerk/nextjs";
+import HamburgerMenu from "./HamburgerMenu";
 
 export default function Navbar() {
-    {/*hamburger menu*/}
     return (
         <div className="flex items-center p-4">
-            <Button variant={"ghost"} size={"icon"} className="md:hidden">
-                <Menu />
-            </Button>
+            <HamburgerMenu />
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/"/>
             </div>
