a0bd248ee07f7c7c256bb3bd9de5a3913c038801
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-04-07 15:49:40 +0530

    initial routing

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <p>Dashboard (Protected)</p>
+      <UserButton afterSignOutUrl="/"/> //to landing page
+    </div>
+  );
+}
