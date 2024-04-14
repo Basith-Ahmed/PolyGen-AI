10da5497b1355a13cb7db934302099f6985e70a7
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-04-14 22:22:33 +0530

    bug-fix

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -78,50 +78,48 @@ export default function ProModal() {
   const proModal = useProModal();
 
   return (
-    <div className="mx-4">
-      <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
-              <div className="flex items-center gap-x-2 font-bold py-1">
-                Upgrade to PolyGen
-                <Badge variant="premium" className="uppercase text-sm py-1">
-                  Pro
-                </Badge>
-              </div>
-              {/* <p className="text-[10px] font-semibold text-zinc-500 -mt-2">get unlimited access to all the models.</p> */}
-            </DialogTitle>
-            <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
-              {tools.map((tool) => (
-                <Card
-                  key={tool.label}
-                  className="p-3 border-black/5 flex items-center justify-between"
-                >
-                  <div className="flex items-center gap-x-4">
-                    <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                      <tool.icon className={cn("w-6 h-6", tool.color)} />
-                    </div>
-                    <div className="font-semibold text-sm">{tool.label}</div>
+    <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+            <div className="flex items-center gap-x-2 font-bold py-1">
+              Upgrade to PolyGen
+              <Badge variant="premium" className="uppercase text-sm py-1">
+                Pro
+              </Badge>
+            </div>
+            {/* <p className="text-[10px] font-semibold text-zinc-500 -mt-2">get unlimited access to all the models.</p> */}
+          </DialogTitle>
+          <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+            {tools.map((tool) => (
+              <Card
+                key={tool.label}
+                className="p-3 border-black/5 flex items-center justify-between"
+              >
+                <div className="flex items-center gap-x-4">
+                  <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                    <tool.icon className={cn("w-6 h-6", tool.color)} />
                   </div>
-                  <CircleCheckBig className="mr-2 text-green-400" />
-                </Card>
-              ))}
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <Button
-              disabled={loading}
-              size="lg"
-              variant="premium"
-              className="w-full"
-              onClick={onSubscribe}
-            >
-              Upgrade
-              <Zap className="w-4 h-4 ml-2 fill-white" />
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
+                  <div className="font-semibold text-sm">{tool.label}</div>
+                </div>
+                <CircleCheckBig className="mr-2 text-green-400" />
+              </Card>
+            ))}
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button
+            disabled={loading}
+            size="lg"
+            variant="premium"
+            className="w-full"
+            onClick={onSubscribe}
+          >
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
